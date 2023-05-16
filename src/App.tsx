1dbafef0059765b801982731d6242f68f0874b4f
alex8023 <alex8023@yandex.ru>
2023-05-16 13:14:34 +0300

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -17,33 +17,10 @@ import UpdateIcon from '@mui/icons-material/Update';
 import { create } from 'zustand';
 import { useChatStore } from './store';
 
-type TInOut = 'in' | 'out';
-
-interface IMsg {
-  textMessage: string;
-  inOut: TInOut;
-  timestamp: number;
-  idMessage: string;
-}
-
-interface IChat {
-  chatId: string;
-  phoneN: string;
-  msgs: IMsg[];
-}
-
 function App() {
-  // const [idInstance, setIdInstance] = useState(''); //1101820479
-  // const [apiToken, setApiToken] = useState(''); // 79be0c51b25848dbbdeb7d5e6a555b337addbd097c844ebea4
   const [phoneN, setPhoneN] = useState('');
   const [message, setMessage] = useState('');
-  // const [chats, setChats] = useState<IChat[]>([
-  //   { chatId: '79100024677@c.us', phoneN: '79100024677', msgs: [] },
-  //   { chatId: '79660358555@c.us', phoneN: '79660358555', msgs: [] },
-  // ]);
   const [activeChat, setActiveChat] = useState('');
-  // const [chatMsgs, setChatMsgs] = useState<any[]>([]);
-
   const chats = useChatStore((st) => st.chats);
   const idInstance = useChatStore((st) => st.idInstance);
   const apiToken = useChatStore((st) => st.apiToken);
@@ -61,7 +38,6 @@ function App() {
         chatId: `${activeChat}@c.us`,
         message,
       }),
-      // redirect: 'follow',
     })
       .then((response) => response.json())
       .then((result) => {
@@ -92,7 +68,6 @@ function App() {
         `https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiToken}`,
         {
           method: 'GET',
-          // redirect: 'follow',
         }
       );
 
@@ -119,14 +94,6 @@ function App() {
         });
       }
 
-      // setChatMsgs((chatMsg) => [...chatMsg, resultRec.body]);
-
-      // .then((response) => response.json())
-      // .then((result) =>
-      //   console.log(result, result.body, result.body.idMessage, result.receiptId)
-      // )
-      // .catch((error) => console.log('error', error));
-
       const responseDel = await fetch(
         `https://api.green-api.com/waInstance${idInstance}/deleteNotification/${apiToken}/${resultRec.receiptId}`,
         {
@@ -139,16 +106,24 @@ function App() {
     }
   }
 
+  function splitPhoneN(str: string) {
+    return [
+      str.slice(0, 1),
+      ' ',
+      str.slice(1, 4),
+      ' ',
+      str.slice(4, 7),
+      ' ',
+      str.slice(7, 9),
+      ' ',
+      str.slice(9, 11),
+    ].join('');
+  }
+
   return (
     <div className={styles.app}>
-      {/* <h1>Green API</h1> */}
       <AppBar position='static' sx={{ padding: '0 20px' }}>
-        <Toolbar
-        // variant='dense'
-        >
-          {/* <IconButton edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton> */}
+        <Toolbar>
           <Typography variant='h6' color='inherit' component='div'>
             GREEN API
           </Typography>
@@ -167,32 +142,26 @@ function App() {
         >
           <TextField
             required
-            // id='outlined-required'
             label='IdInstance'
             value={idInstance}
             onChange={(e) => {
               addIdInstance(e.target.value);
-              // setIdInstance(e.target.value);
             }}
             size='small'
           />
           <TextField
             required
-            // id='outlined-required'
             label='ApiTokenInstance'
             value={apiToken}
             onChange={(e) => {
               addApiToken(e.target.value);
-              // setApiToken(e.target.value);
             }}
             sx={{ width: 500 }}
             size='small'
           />
         </Paper>
         <Box display={'flex'}>
-          <Paper
-          // sx={{ minWidth: 310, height: 'calc(100vh - 220px)', overflow: 'auto' }}
-          >
+          <Paper>
             <Paper
               component='form'
               elevation={3}
@@ -205,10 +174,6 @@ function App() {
               }}
               onSubmit={(e) => {
                 e.preventDefault();
-                // setChats((chats) => [
-                //   { chatId: `${phoneN}@c.us`, phoneN, msgs: [] },
-                //   ...chats,
-                // ]);
                 addChat(phoneN);
                 setPhoneN('');
               }}
@@ -218,10 +183,6 @@ function App() {
                 sx={{ ml: 1, flex: 1 }}
                 placeholder='номер телефона'
                 inputProps={{ 'aria-label': 'input task' }}
-                // value={newTask.taskText}
-                // onChange={(e) => {
-                //   setNewTask((task) => ({ ...task, taskText: e.target.value }));
-                // }}
                 value={phoneN}
                 onChange={(e) => {
                   setPhoneN(e.target.value);
@@ -237,16 +198,10 @@ function App() {
                 <AddCircleOutlineIcon />
               </IconButton>
             </Paper>
-            {/* <ul>
-              {chats.map((chat) => (
-                <li key={chat}>{chat}</li>
-              ))}
-            </ul> */}
             <List sx={{ minWidth: 310, height: 'calc(100vh - 280px)', overflow: 'auto' }}>
               <TransitionGroup>
                 {chats.map((chat) => (
                   <Collapse key={chat.chatId}>
-                    {/* <Task task={task} key={task.id} index={index} /> */}
                     <ListItem
                       key={chat.chatId}
                       onClick={() => {
@@ -260,18 +215,7 @@ function App() {
                             : { px: 5, py: 2 }
                         }
                       >
-                        {/* {chat.phoneN} */}
-                        {[
-                          chat.phoneN.slice(0, 1),
-                          ' ',
-                          chat.phoneN.slice(1, 4),
-                          ' ',
-                          chat.phoneN.slice(4, 7),
-                          ' ',
-                          chat.phoneN.slice(7, 9),
-                          ' ',
-                          chat.phoneN.slice(9, 11),
-                        ].join('')}
+                        {splitPhoneN(chat.phoneN)}
                       </Paper>
                     </ListItem>
                   </Collapse>
@@ -296,18 +240,7 @@ function App() {
                 component='h4'
                 sx={{ whiteSpace: 'nowrap', lineHeight: 1 }}
               >
-                Чат с{' '}
-                {[
-                  activeChat.slice(0, 1),
-                  ' ',
-                  activeChat.slice(1, 4),
-                  ' ',
-                  activeChat.slice(4, 7),
-                  ' ',
-                  activeChat.slice(7, 9),
-                  ' ',
-                  activeChat.slice(9, 11),
-                ].join('')}
+                Чат с {splitPhoneN(activeChat)}
               </Typography>
               <IconButton
                 color='primary'
@@ -318,7 +251,6 @@ function App() {
               </IconButton>
             </Paper>
             <Paper
-              // sx={{ height: '80%', flexGrow: 1 }}
               sx={{ minWidth: 310, height: 'calc(100vh - 300px)', overflow: 'auto' }}
             >
               <List>
@@ -360,7 +292,6 @@ function App() {
               <TextField
                 id='standard-multiline-flexible'
                 placeholder='Введите сообщение'
-                // multiline
                 maxRows={10}
                 sx={{ flexGrow: 1, backgroundColor: 'white' }}
                 value={message}
@@ -368,13 +299,7 @@ function App() {
                   setMessage(e.target.value);
                 }}
               />
-              <Button
-                type='submit'
-                // variant='contained'
-                // endIcon={<SendIcon />}
-                sx={{ alignSelf: 'flex-end' }}
-                // onClick={sendMsg}
-              >
+              <Button type='submit' sx={{ alignSelf: 'flex-end' }}>
                 <SendIcon />
               </Button>
             </Box>
